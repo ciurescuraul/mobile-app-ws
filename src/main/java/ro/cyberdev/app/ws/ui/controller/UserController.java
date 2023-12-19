1aa820d7968ecf63f10089b784f2d1c460d30efc
Krystian <24556350+ciurescuraul@users.noreply.github.com>
2023-12-19 17:45:21 +0200

    return Object as JSON or XML representation

--- src/main/java/ro/cyberdev/app/ws/ui/controller/UserController.java
+++ src/main/java/ro/cyberdev/app/ws/ui/controller/UserController.java
@@ -1,5 +1,6 @@
 package ro.cyberdev.app.ws.ui.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,21 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 import ro.cyberdev.app.ws.ui.model.response.UserRest;
 
 @RestController
-@RequestMapping("/users") // http://localhost:8080/users
+@RequestMapping("/users")
 public class UserController {
 
     @GetMapping
     public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
                            @RequestParam(value = "limit", defaultValue = "50") int limit,
                            @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
-
         return "get users was called page = " + page + " and limit = " + limit + " and sort = " + sort;
     }
 
-    @GetMapping(path = "/{userId}")
+    @GetMapping(path = "/{userId}",
+            produces = {
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE})
     public UserRest getUser(@PathVariable String userId) {
         UserRest returnValue = new UserRest();
-//        returnValue.setUserId(userId);
         returnValue.setFirstName("Ion");
         returnValue.setLastName("Popescu");
         returnValue.setEmail("ion-popescu@mail.com");
